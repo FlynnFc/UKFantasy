e9b989dd686016ab318e7f6c01e9b2fdaae19de2
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-21 21:24:49 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,26 @@ import "../styles/globals.css";
 import type { AppType } from "next/app";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <title>UKFantasy</title>
+        <meta name="description" content="UK Fantasy League" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <SessionProvider session={session}>
+        <Navbar />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
