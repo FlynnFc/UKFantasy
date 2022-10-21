90253fb38ec461ceb7906af979e50246312456d0
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-21 21:24:47 +0100

    Update server.mjs

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -8,14 +8,17 @@ import { env as clientEnv, formatErrors } from "./client.mjs";
 
 const _serverEnv = serverSchema.safeParse(process.env);
 
-if (!_serverEnv.success) {
+if (_serverEnv.success === false) {
   console.error(
     "❌ Invalid environment variables:\n",
-    ...formatErrors(_serverEnv.error.format()),
+    ...formatErrors(_serverEnv.error.format())
   );
   throw new Error("Invalid environment variables");
 }
 
+/**
+ * Validate that server-side environment variables are not exposed to the client.
+ */
 for (let key of Object.keys(_serverEnv.data)) {
   if (key.startsWith("NEXT_PUBLIC_")) {
     console.warn("❌ You are exposing a server-side env-variable:", key);
