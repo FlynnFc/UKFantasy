8d59a62d1d48fdf6f80c7bb94820eb5acd428a21
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-21 21:24:51 +0100

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,32 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "../../../server/db/client";
-import { env } from "../../../env/server.mjs";
+import { prisma} from "../../../server/db/client";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
+
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [   GoogleProvider({
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    clientId: process.env.GOOGLE_ID!,
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    clientSecret: process.env.GOOGLE_SECRET!,
+    authorization: {
+      params: {
+        prompt: "consent",
+        access_type: "offline",
+        response_type: "code"
+      }
+    },
+  }),],
+  pages:{
+    signIn: "/auth/signin"
+  },
+  secret: process.env.sercet,  
+
   // Include user.id on session
   callbacks: {
     session({ session, user }) {
@@ -16,15 +36,6 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
 };
 
 export default NextAuth(authOptions);
