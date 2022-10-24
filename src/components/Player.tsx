fde006e43b4be2bf53ea8ce106b45b1e974fcf56
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-24 23:28:57 +0100

    player component

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Player = () => {
+  const [stats, setStats] = useState(false);
   return (
     <div id="player" className="z-0 h-96 w-72 overflow-hidden bg-orange-500">
       <div
+        onMouseEnter={() => {
+          console.log(stats);
+          setStats(true);
+        }}
+        onMouseLeave={() => {
+          console.log(stats);
+          setStats(false);
+        }}
         id="image"
-        className="z-10 flex h-3/4 items-center justify-center bg-green-500"
+        className="z-10 flex h-72 items-center justify-center bg-green-500"
       >
         image
       </div>
 
-      <div className="flex flex-col items-center justify-evenly bg-yellow-500">
+      <div className="flex h-[6rem] flex-col items-center justify-evenly bg-yellow-500">
         <h2 className="p-2 text-center text-3xl font-bold">Smooya</h2>
         <div>
           <p className="text-center text-2xl">£34,000</p>
         </div>
       </div>
-      <div id="stats" className="h-full w-full transition-transform">
+      <div
+        id="stats"
+        className={`${
+          stats ? "-translate-y-[100%]" : "translate-y-[105%]"
+        }h-full w-full transition-transform`}
+      >
         test
       </div>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: ["-translate-y-[100%]", "translate-y-[105%]"],
   theme: {
     extend: {},
   },
