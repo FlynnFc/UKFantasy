15a681d0e932baeef8fc6b9e7d4e1d9f3cdb023b
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-24 23:43:12 +0100

    Added a "My team page"

---
+++ src/pages/team.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Player } from "../components/Player";
+
+const Team = () => {
+  return (
+    <div className="flex h-full flex-row items-center justify-center space-x-5">
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+      <Player />
+    </div>
+  );
+};
+
+export default Team;
