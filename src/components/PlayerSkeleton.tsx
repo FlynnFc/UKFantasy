7413239556adb183c16ab21434330287f2d66492
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-25 21:40:59 +0100

    Create PlayerSkeleton.tsx

---
+++ src/components/PlayerSkeleton.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import test from "../images/smooya.webp";
+
+const PlayerSkeleton = () => {
+  const [stats, setStats] = useState(false);
+
+  return (
+    <div
+      id="player"
+      className={`z-0 h-72 w-56 overflow-hidden rounded-xl bg-neutral shadow-lg ${
+        stats && "animate-pulse"
+      }`}
+      onMouseEnter={() => {
+        setTimeout(() => setStats(true), 100);
+      }}
+      onMouseLeave={() => {
+        setTimeout(() => setStats(false), 100);
+      }}
+    >
+      <div
+        id="image"
+        className="z-10  h-52 justify-center overflow-hidden bg-base-300"
+      ></div>
+      <div className="flex h-[5rem]  select-none flex-col items-center justify-evenly rounded-b-lg ">
+        <h2 className=" pt-2 text-center text-2xl font-bold leading-none"></h2>
+        <div>
+          <p className="pb-2 text-center text-2xl"></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PlayerSkeleton;
