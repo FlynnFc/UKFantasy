828a99630ba4013415554c1fafd072cde071a995
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-25 04:28:47 +0100

    Update Player.tsx

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,38 +1,41 @@
+import Image from "next/image";
 import React, { useState } from "react";
-
+import test from "../images/smooya.webp";
 export const Player = () => {
   const [stats, setStats] = useState(false);
   return (
     <div
       id="player"
-      className="z-0 h-96 w-72 overflow-hidden rounded-xl bg-orange-500 shadow-lg"
-      onMouseEnter={() => {
-        setTimeout(() => setStats(true), 100);
-      }}
-      onMouseLeave={() => {
-        setTimeout(() => setStats(false), 100);
-      }}
+      className="z-0 h-96 w-72 overflow-hidden rounded-xl bg-neutral shadow-lg"
     >
       <div
         id="image"
-        className="z-10 flex h-72 items-center justify-center bg-green-500"
+        className="z-10  h-72 cursor-pointer justify-center overflow-hidden"
+        onMouseEnter={() => {
+          setTimeout(() => setStats(true), 100);
+        }}
+        onMouseLeave={() => {
+          setTimeout(() => setStats(false), 100);
+        }}
       >
-        image
-      </div>
-
-      <div className="flex h-[6rem] flex-col items-center justify-evenly bg-yellow-500">
-        <h2 className="p-2 text-center text-3xl font-bold">Smooya</h2>
-        <div>
-          <p className="text-center text-2xl">£34,000</p>
+        <Image alt="portrait" src={test} />
+        <div
+          id="stats"
+          className={`${stats && "-translate-y-[106%]"} h-full w-full p-2`}
+        >
+          test
         </div>
       </div>
       <div
-        id="stats"
-        className={`${
-          stats && "-translate-y-[100%]"
-        } h-full w-full transition-transform`}
+        id="nameplate"
+        className="flex h-[6rem] cursor-grab select-none flex-col items-center justify-evenly "
       >
-        test
+        <h2 className=" pt-2 text-center text-3xl font-bold leading-none">
+          Smooya
+        </h2>
+        <div>
+          <p className="pb-2 text-center text-2xl">£34,000</p>
+        </div>
       </div>
     </div>
   );
