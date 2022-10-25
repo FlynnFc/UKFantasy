e94a9bdcf783afb9ac320f93fbcd0aceea7c33a8
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-25 05:00:23 +0100

    Update team.tsx

--- src/pages/[userid]/team.tsx
+++ src/pages/[userid]/team.tsx
@@ -4,11 +4,11 @@ import { Player } from "../../components/Player";
 const Team = () => {
   return (
     <div className="flex h-screen flex-row items-center justify-center space-x-5">
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
+      <Player rareity="gold" name="Smooya" price="35,000" img="test" />
+      <Player rareity="silver" name="StayX" price="22,000" img="test" />
+      <Player rareity="bronze" name="Dweg" price="5,000" img="test" />
+      <Player rareity="silver" name="Ralphy" price="20,000" img="test" />
+      <Player rareity="silver" name="Klon" price="1,000,000" img="test" />
     </div>
   );
 };
