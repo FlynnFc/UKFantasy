5f46ceccde3125e6decabb9a96c43f66175ba159
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-25 17:33:19 +0100

    Create create.tsx

---
+++ src/pages/create.tsx
@@ -0,0 +1,59 @@
+import { useSession } from "next-auth/react";
+import React, { useState } from "react";
+import NotSignedin from "../components/NotSignedin";
+
+const Create = () => {
+  const [introModal, setIntroModal] = useState(true);
+  const session = useSession();
+  console.log(session.data);
+  return (
+    <main className="container mt-2 flex max-h-screen flex-col items-start justify-start p-4">
+      {introModal && (
+        <div className="createModal fixed top-0 left-0 z-20 my-2 flex min-h-screen w-full items-start justify-center bg-info">
+          <div className="mt-32 w-[80%] rounded-lg bg-neutral p-10">
+            <h1 className="text-3xl font-bold leading-loose">
+              Welcome to team creatation
+            </h1>
+            <p className="mb-2 py-2">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+              accusamus expedita facere ex voluptatem exercitationem veniam
+              itaque est repellendus libero neque, culpa distinctio aliquam
+              dolor atque natus esse non nisi!
+            </p>
+            <h2 className="text-2xl font-bold leading-loose">
+              How do points work?
+            </h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+              accusamus expedita facere ex voluptatem exercitationem veniam
+              itaque est repellendus libero neque, culpa distinctio aliquam
+              dolor atque natus esse non nisi!
+            </p>
+            <div className="flex justify-end">
+              <button
+                onClick={() => setIntroModal(false)}
+                className="btn-outline btn"
+              >
+                Got it!
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      {session.data ? (
+        <div className="mt-20">
+          <h1>TEST TITLE</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            alias cupiditate est nam in nihil eaque laboriosam delectus repellat
+            illo voluptate dignissimos accusantium eius maxime, natus dolor
+            velit! Accusantium, ea!
+          </p>
+        </div>
+      ) : (
+        <NotSignedin />
+      )}
+    </main>
+  );
+};
+export default Create;
