ba436296bfada435673ceb082fa35f66060a8d7b
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-25 23:36:19 +0100

    new scroll behaviour

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -4,20 +4,43 @@ import NotSignedin from "../components/NotSignedin";
 import { Player } from "../components/Player";
 import PlayerGroup from "../components/playerGroup";
 import PlayerGroupSkeleton from "../components/playerGroupSkeleton";
+import SelectedPlayer from "../components/SelectedPlayer";
+import lvn from "../images/lvn.webp";
+import dweg from "../images/dweg.webp";
+import smooya from "../images/smooya.webp";
+import thomas from "../images/thomas.webp";
+import vacancey from "../images/vacancey.webp";
 
 const Create = () => {
   const [introModal, setIntroModal] = useState(true);
+  const [myTeam, setMyTeam] = useState([
+    <SelectedPlayer
+      rareity="gold"
+      name="Smooya"
+      price="35,000"
+      img={smooya}
+      key={0}
+    />,
+    <SelectedPlayer
+      rareity="bronze"
+      name="Dweg"
+      price="3.30"
+      img={dweg}
+      key={1}
+    />,
+  ]);
+  const [money, setMoney] = useState(100000);
   const session = useSession();
   console.log(session.data);
-  const test = [
-    <Player rareity="bronze" name="Dweg" price="5,000" img="test" key={0} />,
-    <Player rareity="bronze" name="Dweg" price="5,000" img="test" key={0} />,
-  ];
+
+  const minusMoney = (price: number) => {
+    setMoney((prev) => prev - price);
+  };
 
   return (
-    <main className="min-w-screen container mx-auto  mt-20 flex max-h-screen max-w-7xl flex-col items-end justify-start  p-4">
+    <main className="min-w-screen container mx-auto  mt-20 flex min-h-screen max-w-7xl flex-col items-end justify-start  p-4">
       {introModal && (
-        <div className="createModal fixed top-0 left-0 z-20 my-2 flex min-h-screen w-full items-start justify-center bg-info">
+        <div className="createModal fixed top-0 left-0 z-20 flex h-screen w-full items-start justify-center bg-info">
           <div className="mt-32 w-[80%] rounded-lg bg-neutral p-10">
             <h1 className="text-3xl font-bold leading-loose">
               Welcome to team creatation
@@ -50,59 +73,86 @@ const Create = () => {
       )}
       {session.data ? (
         <div className="w-full">
-          <h1 className="text-3xl leading-snug">Making your team</h1>
+          <h1
+            onClick={() => minusMoney(20000)}
+            className="text-5xl leading-snug"
+          >
+            Making your team
+          </h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
             alias cupiditate est nam in nihil eaque laboriosam delectus repellat
             illo voluptate dignissimos accusantium eius maxime, natus dolor
             velit! Accusantium, ea!
           </p>
-          <div className="my-4">
-            <PlayerGroupSkeleton>{test}</PlayerGroupSkeleton>
+          <div className="sticky top-0 z-10 my-4 ">
+            <PlayerGroupSkeleton money={money}>{myTeam}</PlayerGroupSkeleton>
           </div>
           <div className="space-y-6">
             <PlayerGroup team="God Squad">
-              <Player rareity="gold" name="Smooya" price="35,000" img="test" />
               <Player
-                rareity="silver"
-                name="Stay X"
-                price="22,000"
-                img="test"
+                rareity="gold"
+                name="Smooya"
+                price="35,000"
+                img={smooya}
               />
-              <Player rareity="bronze" name="Dweg" price="5,000" img="test" />
+              <Player rareity="silver" name="LVN" price="22,000" img={lvn} />
+              <Player rareity="bronze" name="Dweg" price="3.30" img={dweg} />
               <Player
-                rareity="silver"
-                name="Ralphy"
-                price="20,000"
-                img="test"
+                rareity="gold"
+                name="Thomas"
+                price="27,000"
+                img={thomas}
               />
               <Player
-                rareity="silver"
-                name="Klon"
-                price="1,000,000"
-                img="test"
+                rareity="gold"
+                name="Vacancey"
+                price="23,000"
+                img={vacancey}
               />
             </PlayerGroup>
-            <PlayerGroup team="Legates">
-              <Player rareity="gold" name="Smooya" price="35,000" img="test" />
+            <PlayerGroup team="God Squad">
               <Player
-                rareity="silver"
-                name="Stay X"
-                price="22,000"
-                img="test"
+                rareity="gold"
+                name="Smooya"
+                price="35,000"
+                img={smooya}
               />
-              <Player rareity="bronze" name="Dweg" price="5,000" img="test" />
+              <Player rareity="silver" name="LVN" price="22,000" img={lvn} />
+              <Player rareity="bronze" name="Dweg" price="3.30" img={dweg} />
               <Player
-                rareity="silver"
-                name="Ralphy"
-                price="20,000"
-                img="test"
+                rareity="gold"
+                name="Thomas"
+                price="27,000"
+                img={thomas}
               />
               <Player
-                rareity="silver"
-                name="Klon"
-                price="1,000,000"
-                img="test"
+                rareity="gold"
+                name="Vacancey"
+                price="23,000"
+                img={vacancey}
+              />
+            </PlayerGroup>
+            <PlayerGroup team="God Squad">
+              <Player
+                rareity="gold"
+                name="Smooya"
+                price="35,000"
+                img={smooya}
+              />
+              <Player rareity="silver" name="LVN" price="22,000" img={lvn} />
+              <Player rareity="bronze" name="Dweg" price="3.30" img={dweg} />
+              <Player
+                rareity="gold"
+                name="Thomas"
+                price="27,000"
+                img={thomas}
+              />
+              <Player
+                rareity="gold"
+                name="Vacancey"
+                price="23,000"
+                img={vacancey}
               />
             </PlayerGroup>
           </div>
