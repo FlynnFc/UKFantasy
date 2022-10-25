8497fee8f60db8a5d0abd4aae7c6d9d9d0d634d4
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-10-25 21:40:56 +0100

    Update playerGroup.tsx

--- src/components/playerGroup.tsx
+++ src/components/playerGroup.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Player } from "./Player";
 
-const PlayerGroup = () => {
+const PlayerGroup = (props: { team: string; children: any[] }) => {
   return (
     <div className="flex w-full flex-col items-center justify-evenly rounded-lg bg-base-300 pb-4">
-      <h2 className="py-2 text-2xl font-bold">Team name</h2>
+      <h2 className="py-2 text-2xl font-bold">{props.team}</h2>
       <div className="flex w-full flex-row justify-evenly">
-        <Player rareity="gold" name="Smooya" price="35,000" img="test" />
-        <Player rareity="silver" name="StayX" price="22,000" img="test" />
-        <Player rareity="bronze" name="Dweg" price="5,000" img="test" />
-        <Player rareity="silver" name="Ralphy" price="20,000" img="test" />
-        <Player rareity="silver" name="Klon" price="1,000,000" img="test" />
+        {props.children.map((child: any) => {
+          return (
+            <Player
+              key={child.props.name}
+              rareity={child.props.rareity}
+              name={child.props.name}
+              price={child.props.price}
+            />
+          );
+        })}
       </div>
     </div>
   );
