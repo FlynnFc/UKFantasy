db81e00fe1097cdb925ceae72c913c653e3b74ae
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-11-22 15:50:30 +0000

    Update signin.tsx

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -7,23 +7,46 @@ export default function SignIn(props: { providers: any }) {
       className="flex h-screen items-center justify-center"
       style={{ overflow: "hidden", position: "relative" }}
     >
-      <div className="flex w-96 items-center justify-center rounded-md bg-primary p-6 shadow-md">
-        {props.providers &&
-          Object.values(props.providers).map((provider: any) => (
-            <div
-              className="flex justify-center"
-              key={provider.name}
-              style={{ marginBottom: 0 }}
-            >
+      <div className="mb-10 flex w-96 flex-col items-center rounded-md bg-base-300 shadow-lg shadow-base-300">
+        <h1 className="pb-4 pt-6 text-3xl font-bold text-base-content">
+          Sign in
+        </h1>
+        <div className="my-2 w-full space-y-2 border-b-2 border-base-100 p-6 pb-4">
+          <p className="text-error">Email coming soon</p>
+          <input
+            type="text"
+            placeholder="Email"
+            className="w-full rounded-lg bg-base-100 p-3"
+          />{" "}
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full rounded-lg bg-base-100 p-3"
+          />
+          <button className="btn-outline btn w-full" disabled>
+            Sign in
+          </button>
+        </div>
+        <div className="flex w-96 items-center justify-center rounded-md bg-primary p-6 shadow-md">
+          {props.providers &&
+            Object.values(props.providers).map((provider: any) => (
               <div
-                className="btn-lg btn"
-                onClick={() => signIn(provider.id, { callbackUrl: "/epic36" })}
+                className="flex justify-center"
+                key={provider.name}
+                style={{ marginBottom: 0 }}
               >
-                Sign in with {provider.name}
-                <FcGoogle className="mx-4 inline" />
+                <div
+                  className="btn-lg btn"
+                  onClick={() =>
+                    signIn(provider.id, { callbackUrl: "/epic36" })
+                  }
+                >
+                  Sign in with {provider.name}
+                  <FcGoogle className="mx-4 inline" />
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
       </div>
     </div>
   );
