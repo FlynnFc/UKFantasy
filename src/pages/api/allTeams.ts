3e267d2f120178e12472859b911a69e3a7cbbc94
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-11-22 14:00:06 +0000

    gets all teams submitted by players

---
+++ src/pages/api/allTeams.ts
@@ -0,0 +1,15 @@
+// src/pages/api/examples.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../server/db/client";
+
+const allTeams = async (req: NextApiRequest, res: NextApiResponse) => {
+  if(req.method !== "GET") {
+    res.status(500).json("Method not authorised")
+    return
+  }
+  const data = await prisma.playerTeam.findMany();
+  
+  res.status(200).json(data);
+};
+
+export default allTeams;
