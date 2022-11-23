9b8835b2b5161e3c4d0957e835ed3bb6f863c1ca
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-11-23 19:30:01 +0000

    Update index.tsx

--- src/pages/[userid]/index.tsx
+++ src/pages/[userid]/index.tsx
@@ -6,11 +6,7 @@ import PlayerGroupSkeleton from "../../components/playerGroupSkeleton";
 const Index = () => {
   const money = 100000;
   const [myTeam, setMyTeam] = useState<JSX.Element[]>([]);
-  return (
-    <div className="mx-5 flex h-screen items-center justify-center">
-      <PlayerGroupSkeleton money={money}></PlayerGroupSkeleton>
-    </div>
-  );
+  return <div className="mx-5 flex h-screen items-center justify-center"></div>;
 };
 
 export default Index;
