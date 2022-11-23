7776631e1c8fb77c600edcc73af538351bed87c1
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-11-23 19:07:01 +0000

    darkmode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,38 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LoginBtn from "./LoginBtn";
+import { themeChange } from "theme-change";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 const Navbar = () => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  const themeHandler = () => {
+    setDarkMode((darkMode) => !darkMode);
+  };
+
+  useEffect(() => {
+    themeChange(false);
+    // 👆 false parameter is required for react project
+  }, []);
+
   return (
     <div className="navbar absolute top-0">
       <div className="flex w-full justify-between">
         <div className="flex">
+          <button
+            onClick={themeHandler}
+            className="btn "
+            data-toggle-theme="winter,night"
+          >
+            {!darkMode && <FaMoon className="text-xl" />}
+            {darkMode && <FaSun className="text-xl" />}
+          </button>
           <Link href="./">
             <a className="btn-ghost btn text-xl normal-case">UKFantasy</a>
           </Link>
         </div>
-        <div className="space-x-2">
+        <div className="flex space-x-2">
           <Link href="/epic36">
             <button className="btn bg-primary">Epic36 </button>
           </Link>
