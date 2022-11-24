8dde3ce5aa62161103d3766f5b3bda8f58b0798f
Flynn FC <88561679+FlynnFc@users.noreply.github.com>
2022-11-24 02:41:07 +0000

    Update client.ts

--- src/server/client.ts
+++ src/server/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 
-const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
+const supabaseUrl = 'https://wosipkxcwhwqrtnbwdxx.supabase.co'
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const supabaseAnonKey = process.env.SUPABASE_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indvc2lwa3hjd2h3cXJ0bmJ3ZHh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjkyMDEzOTYsImV4cCI6MTk4NDc3NzM5Nn0.nAUbNC0ZpQMEfsGfvgT_uINqbSfyAoZ62ITEklUl2Os' 
+console.log(supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
