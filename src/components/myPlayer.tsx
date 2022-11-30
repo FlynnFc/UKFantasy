9676ac7f2cbe6158245751cdd0ff3a2c9d9a0b7a
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-11-30 01:08:48 +0000

    Update myPlayer.tsx

--- src/components/myPlayer.tsx
+++ src/components/myPlayer.tsx
@@ -5,7 +5,7 @@ type myPlayer = {
   name: string;
   price: number;
   rareity: string;
-  img?: any;
+  img?: string;
 };
 
 export const MyPlayer = (props: myPlayer) => {
@@ -18,10 +18,10 @@ export const MyPlayer = (props: myPlayer) => {
 
   return (
     <div
-      className={`relative z-0 w-56 overflow-hidden rounded-xl bg-neutral shadow-lg lg:h-72`}
+      className={`relative z-0 h-auto w-[18.7rem] overflow-hidden rounded-xl bg-neutral shadow-lg`}
     >
       <div
-        className={`z-10 hidden h-52 cursor-auto justify-center overflow-hidden lg:block`}
+        className={`z-10 m-0 hidden h-auto cursor-auto justify-center overflow-hidden lg:flex`}
         onMouseEnter={() => {
           setTimeout(() => setStats(true), 100);
         }}
@@ -29,38 +29,22 @@ export const MyPlayer = (props: myPlayer) => {
           setTimeout(() => setStats(false), 100);
         }}
       >
-        {/* <Image
+        <Image
           className="drop-shadow-2xl"
           alt="portrait"
           height={300}
           width={300}
-          src={props.img}
-        /> */}
-        <div
-          className={`${
-            stats && "h-full w-full -translate-y-[111%] p-2"
-          } "h-full p-2" stats w-full text-base-content `}
-        >
-          <ul className="flex h-full flex-col justify-between">
-            <div>
-              <li>HLTV: N/A</li>
-              <li>Faceit Elo: 3400 </li>
-              <li>HS%: 54.3%</li>
-              <li>Entry Rounds: 10.4%</li>
-              <li>Clutch Rounds: 0.4%</li>
-            </div>
-            <button className="btn">Detailed Stats</button>
-          </ul>
-        </div>
+          src="/_next/static/media/dweg.0d76dcc6.webp"
+        />
       </div>
       <div
-        className={`${rareity} flex h-[5rem] select-none flex-col items-center justify-evenly rounded-b-lg `}
+        className={`${rareity} flex h-auto select-none flex-col items-center justify-evenly rounded-b-lg `}
       >
-        <h2 className=" pt-2 text-center font-bold leading-none text-neutral lg:text-2xl">
+        <h2 className="pt-2 text-center font-bold leading-none text-neutral md:text-xl xl:text-3xl">
           {props.name}
         </h2>
         <div>
-          <p className="pb-2 text-center text-neutral lg:text-2xl">
+          <p className="pb-2 text-center text-neutral lg:text-3xl">
             £{props.price}
           </p>
         </div>
