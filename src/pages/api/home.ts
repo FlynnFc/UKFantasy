b5b2f8a618c39d5bf4daebf8001de1b4b6be0a88
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2022-11-30 01:09:13 +0000

    setup for homepage

---
+++ src/pages/api/home.ts
@@ -0,0 +1,15 @@
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../server/db/client";
+
+const allTeams = async (req: NextApiRequest, res: NextApiResponse) => {
+  if(req.method !== "GET") {
+    res.status(500).json("Method not authorised")
+    return
+  }
+  const data = await prisma.playerTeam.findMany()
+  console.log(data)
+  res.status(200).json(data);
+};
+
+export default allTeams;
