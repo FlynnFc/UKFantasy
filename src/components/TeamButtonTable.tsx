636a5d56ea6613e8d4e68858ab6d56e909cdf221
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2023-01-10 01:47:00 +0000

    Update TeamButtonTable.tsx

--- src/components/TeamButtonTable.tsx
+++ src/components/TeamButtonTable.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import React from "react";
 
-const TeamButtonTable = (props: { name: string; id: string }) => {
+const TeamButtonTable = (props: {
+  name: string;
+  id: string;
+  league: string;
+}) => {
   return (
     <>
-      <Link href={`/team/${props.id}`}>
+      <Link href={`${props.league}/team/${props.id}`}>
         <button className="rounded p-2 transition-none hover:bg-base-300">
           {props.name}
         </button>
