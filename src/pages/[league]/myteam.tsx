e3474bc735e1ecc1e46f88794f99fa44169f0bd2
Flynn Foley-Clarke <mastare.flynn@gmail.com>
2023-01-10 01:47:40 +0000

    Create myteam.tsx

---
+++ src/pages/[league]/myteam.tsx
@@ -0,0 +1,101 @@
+import { useSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
+import Loading from "../../components/Loading";
+import { FiShare } from "react-icons/fi";
+import { MyPlayer } from "../../components/myPlayer";
+import toast, { Toaster } from "react-hot-toast";
+import { useRouter } from "next/router";
+
+type player = {
+  id: string;
+  name: string;
+  price: number;
+  Rareity: string;
+  teamId: string;
+  statsId: string;
+  Image: string;
+};
+
+type playerTeam = {
+  id: string;
+  points: string;
+  Player: player[];
+  teamName: string;
+};
+
+type teamProps = {
+  PlayerTeam: playerTeam[];
+  email: string;
+  name: string;
+};
+
+const Myteam = () => {
+  const { data: session } = useSession();
+  const [team, setTeam] = useState<teamProps>();
+  const { query } = useRouter();
+  useEffect(() => {
+    const fetcher = async () => {
+      if (session?.user?.id) {
+        const id = session.user.id;
+        const res = await fetch("/api/myTeam", {
+          method: "GET",
+          headers: { id: id },
+        });
+        if (!res.ok) {
+          console.log("error");
+        }
+        const data = await res.json();
+
+        setTeam(data);
+        console.log(team);
+      } else return "error";
+    };
+
+    fetcher();
+
+    console.log("running");
+  }, [session]);
+
+  const linkSetter = () => {
+    const path: string | any = team?.PlayerTeam[0]?.id;
+    const host = `https://uk-fantasy.vercel.app/${query.league}/team/`;
+    const link = host + path;
+    navigator.clipboard.writeText(link);
+    toast.success("added link to clipboard");
+  };
+  return (
+    <main className="min-w-screen container mx-auto flex h-screen min-h-[88.3vh] max-w-7xl flex-col items-center justify-start  p-4">
+      <Toaster />
+      {team ? (
+        <div className="flex flex-col items-center justify-center ">
+          <div className="flex flex-row items-center space-x-2 sm:mb-10">
+            <h1 className=" mb-2 text-4xl ">{team?.PlayerTeam[0]?.teamName}</h1>
+
+            <button onClick={linkSetter} className="mb-1 p-2 text-2xl">
+              <FiShare />
+            </button>
+          </div>
+
+          <div className="flex h-auto flex-col items-center justify-between space-y-2 rounded-lg bg-base-300 p-6 sm:max-w-[80vw] sm:flex-row sm:space-y-0 sm:space-x-4">
+            {team &&
+              team.PlayerTeam[0]?.Player?.map((el) => {
+                return (
+                  <MyPlayer
+                    key={el.id}
+                    name={el.name}
+                    price={el.price}
+                    rareity={el.Rareity}
+                    img={el.Image}
+                  />
+                );
+              })}
+          </div>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </main>
+  );
+};
+
+export default Myteam;
