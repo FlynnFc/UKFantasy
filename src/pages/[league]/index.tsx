7c8fd0de80a383ccab7caaeb55fcb67fec247e04
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-02-22 15:23:25 +0000

    Update index.tsx

--- src/pages/[league]/index.tsx
+++ src/pages/[league]/index.tsx
@@ -12,7 +12,7 @@ import Loading from "../../components/Loading";
 import LoginBtn from "../../components/LoginBtn";
 import Table from "../../components/Table";
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const res = await fetch("https://uk-fantasy.vercel.app/api/allUserTeams");
   const data = await res.json();
   return {
@@ -23,19 +23,19 @@ export async function getStaticProps() {
   };
 }
 
-export async function getStaticPaths() {
-  const path = "https://uk-fantasy.vercel.app/";
-  const res = await fetch(`${path}/api/allLeagues`, { method: "GET" });
-  const data = await res.json();
-  const paths = data.map((league: { name: string }) => ({
-    params: { league: league.name.toLowerCase() },
-  }));
+// export async function getStaticPaths() {
+//   const path = "https://uk-fantasy.vercel.app/";
+//   const res = await fetch(`${path}/api/allLeagues`, { method: "GET" });
+//   const data = await res.json();
+//   const paths = data.map((league: { name: string }) => ({
+//     params: { league: league.name.toLowerCase() },
+//   }));
 
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
 type UserProps = {
   User: [{ id: string }];
