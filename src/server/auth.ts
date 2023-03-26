b08ee67af75d029e77065aa1de59489af0159607
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-03-26 00:06:14 +0000

    Update auth.ts

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -20,15 +20,6 @@ declare module "next-auth" {
 
 
 const authOptions: NextAuthOptions = {
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-        // session.user.role = user.role; <-- put other properties on the session here
-      }
-      return session;
-    },
-  },
   adapter: PrismaAdapter(prisma),
   providers: [
     SteamProvider({
