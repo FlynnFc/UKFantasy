17bba95849ff3ffa122957f9ba90b6f76afc96fa
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-03-28 03:58:47 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,13 +15,13 @@ const Home: NextPage = () => {
   // }, [router, status]);
   return (
     <>
-      <main className="container mx-auto flex min-h-screen flex-col items-center justify-between p-4">
+      <main className="container mx-auto flex min-h-screen flex-col items-center justify-start p-4">
         <h1 className="my-2 w-5/6 text-center text-2xl font-semibold md:text-4xl xl:text-[6rem] xl:leading-tight">
           Welcome to the UK CS{" "}
           <span className="font-bold text-accent"> Fantasy League</span>!
         </h1>
 
-        <div className="flex w-full flex-col items-center">
+        <div className="my-5 flex w-full flex-col items-center">
           <Link href={"./epic39"}>
             <button className="btn-primary btn-lg btn w-fit">
               Take a peek
