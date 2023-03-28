1e9c55850caec8e39873613ba657008b4da2c340
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-03-28 03:34:26 +0100

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react/dist'
 
 export default defineConfig({
+  plugins: [react()],
   test: {
-    // ...
+    environment: 'jsdom',
   },
 })
