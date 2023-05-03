f46ba625963f5ff2fe0375b1071feeb3cdd633eb
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-05-03 21:59:36 +0100

    Update [userTeamid].tsx

--- src/pages/[league]/team/[userTeamid].tsx
+++ src/pages/[league]/team/[userTeamid].tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 import Loading from "../../../components/Loading";
 
 import { MyPlayer } from "../../../components/myPlayer";
+import Head from "next/head";
 
 type player = {
   bonus: { name: string; description: string };
@@ -50,35 +51,41 @@ const UserTeam = () => {
   console.log(team);
 
   return (
-    <main className="min-w-screen container mx-auto flex h-screen min-h-[88.3vh] max-w-7xl flex-col items-center justify-start  p-4">
-      {team ? (
-        <div className="flex flex-col">
-          <h1 className="mb-4 text-center text-4xl">{team.teamName}</h1>
-          <h2 className="m-2 mx-2 text-center text-lg">{`${team.User.name}'s team`}</h2>
-          <div className="flex h-auto flex-col items-center justify-between space-y-2 rounded-lg bg-base-300 p-6 sm:max-w-[80vw] sm:flex-row sm:space-y-0 sm:space-x-4">
-            {team.SelectedPlayer.map((el) => {
-              return (
-                <MyPlayer
-                  key={el.id}
-                  name={el.name}
-                  price={el.price}
-                  rareity={el.rareity}
-                  img={el.image}
-                  bonus={el.bonus}
-                  index={0}
-                  deleteBonus={function (i: number): void {
-                    throw new Error("Function not implemented.");
-                  }}
-                  bonusEdit={false}
-                />
-              );
-            })}
+    <>
+      <Head>
+        <title>{`${team?.User.name}'s team`}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="min-w-screen container mx-auto flex h-screen min-h-[88.3vh] max-w-7xl flex-col items-center justify-start  p-4">
+        {team ? (
+          <div className="flex flex-col">
+            <h1 className="mb-4 text-center text-4xl">{team.teamName}</h1>
+            <h2 className="m-2 mx-2 text-center text-lg">{`${team.User.name}'s team`}</h2>
+            <div className="flex h-auto flex-col items-center justify-between space-y-2 rounded-lg bg-base-300 p-6 sm:max-w-[80vw] sm:flex-row sm:space-x-4 sm:space-y-0">
+              {team.SelectedPlayer.map((el) => {
+                return (
+                  <MyPlayer
+                    key={el.id}
+                    name={el.name}
+                    price={el.price}
+                    rareity={el.rareity}
+                    img={el.image}
+                    bonus={el.bonus}
+                    index={0}
+                    deleteBonus={function (i: number): void {
+                      throw new Error("Function not implemented.");
+                    }}
+                    bonusEdit={false}
+                  />
+                );
+              })}
+            </div>
           </div>
-        </div>
-      ) : (
-        <Loading />
-      )}
-    </main>
+        ) : (
+          <Loading />
+        )}
+      </main>
+    </>
   );
 };
 
