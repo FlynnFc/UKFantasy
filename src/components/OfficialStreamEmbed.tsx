b9996902422485cb002062cd95cc444181bac5ea
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-05-14 19:22:21 +0100

    Update OfficialStreamEmbed.tsx

--- src/components/OfficialStreamEmbed.tsx
+++ src/components/OfficialStreamEmbed.tsx
@@ -4,12 +4,9 @@ const OfficialStreamEmbed = () => {
   return (
     <div
       draggable
-      className={`collapse-arrow rounded-btn collapse fixed bottom-1 left-1 z-20 mt-5 hidden h-max w-max bg-secondary text-secondary-content shadow-lg lg:grid`}
+      className={`collapse-plus rounded-btn collapse fixed bottom-1 left-1 z-20 mt-5 hidden h-max w-max bg-secondary text-secondary-content shadow-lg transition-all lg:grid`}
     >
       <input type="checkbox" className="peer" />
-      <div className="collapse-title flex justify-between">
-        <h4 className="text-lg">The official stream is Live right now!</h4>
-      </div>
       <div className="collapse-content">
         <iframe
           src="https://player.twitch.tv/?channel=epiclan1&parent=esportsfantasy.app"
@@ -19,6 +16,9 @@ const OfficialStreamEmbed = () => {
         >
           iframe is not support in your browser
         </iframe>
+      </div>
+      <div className="collapse-title flex justify-between">
+        <h4 className="text-lg">The official stream is Live right now!</h4>
       </div>
     </div>
   );
