62b9ec7be37e05bd705d71b0e260c430157bc621
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-05-14 18:32:56 +0100

    stream embed change

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading = () => {
   return (
     <>
-      <div className="fixed bottom-2 left-2 z-20 flex animate-bounce flex-row rounded-lg bg-primary p-4">
+      <div className="fixed left-1/2 top-2 z-20 flex animate-bounce flex-row rounded-lg bg-primary p-4">
         <h3 className="text-xl font-bold text-primary-content">Loading...</h3>
       </div>
     </>
