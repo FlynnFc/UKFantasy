eac46ee55bbe1f57789ebd566b96b9d1507e544b
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-06-02 19:20:25 +0100

    Update index.tsx

--- src/pages/[league]/points/index.tsx
+++ src/pages/[league]/points/index.tsx
@@ -5,10 +5,10 @@ const index = () => {
     <div className="min-w-screen container flex h-full min-h-[88.3vh]  max-w-7xl select-none flex-col items-center justify-start sm:mx-auto">
       <h1 className="text-3xl">Epic39 point dashboard</h1>
       <div className="join">
-        <button className="join-item ">Round 1</button>
-        <button className="join-item ">Round 2</button>
-        <button className="join-item ">Round 3</button>
-        <button className="join-item ">Round 4</button>
+        <button className="join-item btn rounded-r-none">Round 1</button>
+        <button className="join-item btn rounded-none">Round 2</button>
+        <button className="join-item btn rounded-none">Round 3</button>
+        <button className="join-item btn rounded-l-none">Round 4</button>
       </div>
     </div>
   );
