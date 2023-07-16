8fba1614009d72b3b225ba7a1861e6ae3a0ba144
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-07-16 21:12:09 +0100

    Create getPosts.ts

---
+++ src/pages/api/getPosts.ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../server/db/client";
+
+const getPosts = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+  const leagueName = req.headers.leaguename as string;
+  switch (method) {
+    case "GET":
+      try {
+        if (!leagueName) {
+          const posts = await prisma.highLightPost.findMany({
+            include: { author: true },
+          });
+          res.status(200).json(posts);
+        } else {
+          const posts = await prisma.highLightPost.findMany({
+            where: { league: { name: leagueName } },
+            include: { author: true },
+          });
+          res.status(200).json(posts);
+        }
+      } catch (e) {
+        console.error("Request error", e);
+        res.status(500).json({ error: "Error fetching highlight posts" });
+      }
+      break;
+    default:
+      res.setHeader("Allow", ["GET"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
+};
+
+export default getPosts;
