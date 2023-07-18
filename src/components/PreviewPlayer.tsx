f0f23cc0f067fa974ba15cb774af74e1924ab151
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-07-18 18:53:16 +0100

    yoo

--- src/components/PreviewPlayer.tsx
+++ src/components/PreviewPlayer.tsx
@@ -13,61 +13,33 @@ export type player = {
 const PreviewPlayer = (props: player) => {
   const [stats, setStats] = useState(false);
   return (
-    <div
-      className={`relative z-0 w-56 overflow-hidden rounded-xl bg-neutral shadow-lg lg:h-72`}
-    >
+    <div className={` flex-col shadow-lg xl:h-[20rem] xl:w-[14rem]`}>
       <div
-        className={`hidden h-52 cursor-auto justify-center lg:block`}
-        onMouseEnter={() => {
-          setTimeout(() => setStats(true), 100);
-        }}
-        onMouseLeave={() => {
-          setTimeout(() => setStats(false), 100);
-        }}
+        className={` relative hidden h-[20rem] cursor-auto rounded-b-none bg-base-200 xl:block`}
       >
+        <div
+          className={`absolute bottom-3 z-10 w-full select-none flex-col items-center justify-evenly justify-self-end font-bold  `}
+        >
+          <div className="lg:hidden">{props.name}</div>
+          <div>
+            <p
+              className={`pb-2 text-center shadow-2xl lg:text-3xl ${props.rareity}`}
+            >
+              £{props.price}
+            </p>
+          </div>
+        </div>
         {props.image ? (
           <Image
             loading="eager"
-            className="text-center drop-shadow-2xl"
+            className="z-0 text-center drop-shadow-2xl"
             alt="player portrait"
-            height={500}
-            width={500}
             src={props.image}
+            layout="fill"
           />
         ) : (
           <div></div>
         )}
-        {props.stats && (
-          <div
-            className={
-              stats
-                ? "playerstats absolute top-0  h-full w-full p-2 text-white"
-                : "playerstats absolute top-full h-full w-full p-2 text-white"
-            }
-          >
-            <ul className="flex flex-col justify-start gap-1">
-              <li>HLTV: {props.stats.hltv}</li>
-              <li>Faceit Elo: {props.stats.elo} </li>
-              <li>KAST: %</li>
-              <li>ADR: 10</li>
-              <li>HS%: {props.stats.hs}%</li>
-              <li>Entry Rounds: {props.stats.entryRounds}%</li>
-              <li>Clutch Rounds: {props.stats.clutchRounds}%</li>
-            </ul>
-          </div>
-        )}
-      </div>
-      <div
-        className={`${props.rareity} relative flex h-[5rem] cursor-pointer select-none flex-col items-center justify-evenly rounded-b-lg `}
-      >
-        <h2 className=" pt-2 text-center font-bold leading-none text-neutral lg:text-2xl">
-          {props.name}
-        </h2>
-        <div>
-          <p className="pb-2 text-center text-neutral lg:text-2xl">
-            £{props.price}
-          </p>
-        </div>
       </div>
     </div>
   );
