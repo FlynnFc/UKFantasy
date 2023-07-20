f74f1be2b2a1c3f2788deaa7ccc42d5298c54a13
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-07-20 16:22:54 +0100

    Update playerGroup.tsx

--- src/components/playerGroup.tsx
+++ src/components/playerGroup.tsx
@@ -15,7 +15,7 @@ const PlayerGroup = (props: {
   )[];
 }) => {
   return (
-    <div className="flex w-full flex-col items-center justify-evenly rounded-lg bg-base-300 pb-4">
+    <div className="flex w-full flex-col items-center justify-evenly rounded-lg bg-base-300 pb-16">
       <div className="relative my-4">
         <span className="mb-2 select-none py-6 text-2xl font-bold">
           {props.team}
