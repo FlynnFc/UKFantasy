62f4c360b9be3c86b966f19d88da8b5e4a139e01
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-07-20 15:32:34 +0100

    works on mobile

--- src/components/PreviewPlayer.tsx
+++ src/components/PreviewPlayer.tsx
@@ -13,16 +13,15 @@ export type player = {
 const PreviewPlayer = (props: player) => {
   const [stats, setStats] = useState(false);
   return (
-    <div className={` flex-col shadow-lg lg:w-[14rem] xl:h-[20rem]`}>
-      <div
-        className={` relative hidden h-[20rem] cursor-auto rounded-b-none bg-base-200 xl:block`}
-      >
+    <div className={` flex-col lg:h-[20rem] lg:w-[14rem] lg:shadow-lg`}>
+      <div className={` relative cursor-auto rounded bg-base-100 lg:h-[20rem]`}>
         <div
-          className={`absolute bottom-3 z-10 w-full select-none flex-col items-center justify-evenly justify-self-end font-bold  `}
+          className={`bottom-3 z-10 w-full select-none flex-col  items-center justify-evenly p-2 font-bold  lg:absolute  `}
         >
           <div>
+            <div className="text-center lg:hidden">{props.name}</div>
             <p
-              className={`flex flex-col pb-2 text-center lg:text-3xl ${
+              className={`z-0 pb-2 text-center lg:text-3xl ${
                 props.price >= 21500
                   ? "gold"
                   : props.price > 19000
@@ -30,26 +29,23 @@ const PreviewPlayer = (props: player) => {
                   : "bronze"
               }`}
             >
-              <span>
-                {props.image ===
-                  "https://wosipkxcwhwqrtnbwdxx.supabase.co/storage/v1/object/sign/players/ghost?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwbGF5ZXJzL2dob3N0IiwiaWF0IjoxNjg5Nzk5MDQxLCJleHAiOjE3MjEzMzUwNDF9.zGDt3amKB3L7hwOoakyIySWv51yDnSOw7m5jvDh4hUE&t=2023-07-19T20%3A37%3A30.001Z" &&
-                  props.name}
-              </span>
               £{props.price}
             </p>
           </div>
         </div>
-        {props.image ? (
-          <Image
-            loading="eager"
-            className="z-0 text-center drop-shadow-2xl"
-            alt="player portrait"
-            src={props.image}
-            layout="fill"
-          />
-        ) : (
-          <div></div>
-        )}
+        <div className={`z-0 hidden cursor-auto justify-center lg:block`}>
+          {props.image ? (
+            <Image
+              loading="eager"
+              className="z-0 text-center drop-shadow-2xl"
+              alt="player portrait"
+              src={props.image}
+              layout="fill"
+            />
+          ) : (
+            <div></div>
+          )}
+        </div>
       </div>
     </div>
   );
