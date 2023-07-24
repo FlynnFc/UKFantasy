caf9c1ad731a03dd2bfdf1a464dff09ffd64e18b
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-07-24 18:33:55 +0100

    Update updatePlayer.ts

--- src/pages/api/updatePlayer.ts
+++ src/pages/api/updatePlayer.ts
@@ -7,15 +7,32 @@ const updatePlayer = async (req: NextApiRequest, res: NextApiResponse) => {
   const { body } = req;
   const id: string = headers.id as string;
   const data = JSON.parse(body);
+  const querys = [];
+
+  if (!id) {
+    for (let index = 0; index < data.length; index++) {
+      const element = data[index];
+      const query = prisma.player.update({
+        where: { steamid: element.steamid },
+        data: { price: element.price },
+      });
+      querys.push(query);
+    }
+  }
 
   switch (method) {
     case "POST":
       try {
-        const player = await prisma.player.update({
-          where: { id: id },
-          data: { ...data },
-        });
-        res.status(200).json(player);
+        if (id) {
+          const player = await prisma.player.update({
+            where: { id: id },
+            data: { ...data },
+          });
+          res.status(200).json(player);
+        } else {
+          const player = await prisma.$transaction(querys);
+          res.status(200).json(player);
+        }
       } catch (e) {
         console.error("Request error", e);
         res.status(500).json({ error: "Error fetching players" });
