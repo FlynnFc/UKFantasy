479eca7f6209a01ff201fc15b81137edf7c9b6e1
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-07-24 12:35:10 +0100

    bosh

--- src/pages/api/updatelikes.ts
+++ src/pages/api/updatelikes.ts
@@ -7,18 +7,23 @@ const postHighlights = async (req: NextApiRequest, res: NextApiResponse) => {
   const data = JSON.parse(body);
 
   const post = {
-    likes: data.likes,
-    id: data.id,
+    likedBy: data.likedBy as string,
+    id: data.id as string,
+    isLiked: data.isLiked,
   };
 
   switch (method) {
     case "POST":
       try {
-        const newHighlight = await prisma.highlight.update({
-          where: { id: post.id },
-          data: { likes: post.likes },
-        });
-        res.status(200).json(newHighlight);
+        if (post.isLiked) {
+          const addHighLightLike = await prisma.highlight.update({
+            where: { id: post.id },
+            data: { highlightLike: { create: { userId: post.likedBy } } },
+          });
+          res.status(200).json(addHighLightLike);
+        } else {
+          res.status(500).json({ error: "Error posting highlight" });
+        }
       } catch (e) {
         console.error("Request error", e);
         res.status(500).json({ error: "Error posting highlight" });
