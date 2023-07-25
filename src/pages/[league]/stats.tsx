820859999451279e722a708f81518a170c8760f3
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-07-25 13:36:39 +0100

    n o more button

--- src/pages/[league]/stats.tsx
+++ src/pages/[league]/stats.tsx
@@ -115,64 +115,65 @@ export async function getStaticPaths() {
 const Stats = (props: { data: any; data2: any }) => {
   const [playerData, setPlayerData] = useState(new Map());
   const [emptyPlayer, setEmptyPlayer] = useState(new Map());
-  const { query } = useRouter();
-
-  const playerDataHandler = async () => {
-    const newPlayerstats = new Map<string, any>();
-    for (let i = 0; i < props.data.length; i++) {
-      const team = props.data[i].SelectedPlayer;
-      for (let j = 0; j < team.length; j++) {
-        const player = team[j];
-        if (newPlayerstats.has(player.name)) {
-          const valss = newPlayerstats.get(player.name);
-          const bonus = player.bonusName;
-          let newval = 1;
-          for (const [key, value] of Object.entries(valss)) {
-            if (key === bonus) {
-              newval = (value as number) + 1;
+  useEffect(() => {
+    const playerDataHandler = () => {
+      const newPlayerstats = new Map<string, any>();
+      for (let i = 0; i < props.data.length; i++) {
+        const team = props.data[i].SelectedPlayer;
+        for (let j = 0; j < team.length; j++) {
+          const player = team[j];
+          if (newPlayerstats.has(player.name)) {
+            const valss = newPlayerstats.get(player.name);
+            const bonus = player.bonusName;
+            let newval = 1;
+            for (const [key, value] of Object.entries(valss)) {
+              if (key === bonus) {
+                newval = (value as number) + 1;
+              }
             }
+            newPlayerstats.set(player.name, {
+              ...newPlayerstats.get(player.name),
+              [bonus]: newval,
+            });
+          } else {
+            newPlayerstats.set(player.name, { [player.bonusName]: 1 });
           }
-          newPlayerstats.set(player.name, {
-            ...newPlayerstats.get(player.name),
-            [bonus]: newval,
-          });
-        } else {
-          newPlayerstats.set(player.name, { [player.bonusName]: 1 });
         }
       }
-    }
 
-    //General player frequency
-    const players = [];
-    for (let i = 0; i < props.data.length; i++) {
-      const team = props.data[i].SelectedPlayer;
-      for (let j = 0; j < team.length; j++) {
-        const player = team[j];
-        players.push(player);
+      //General player frequency
+      const players = [];
+      for (let i = 0; i < props.data.length; i++) {
+        const team = props.data[i].SelectedPlayer;
+        for (let j = 0; j < team.length; j++) {
+          const player = team[j];
+          players.push(player);
+        }
       }
-    }
-    const pickFreq = new Map<string, number>();
-    for (let i = 0; i < props.data2.Teams.length; i++) {
-      const team = props.data2.Teams[i];
+      const pickFreq = new Map<string, number>();
+      for (let i = 0; i < props.data2.Teams.length; i++) {
+        const team = props.data2.Teams[i];
 
-      for (let j = 0; j < team.Player.length; j++) {
-        const player = team.Player[j];
+        for (let j = 0; j < team.Player.length; j++) {
+          const player = team.Player[j];
 
-        pickFreq.set(player.name, 0);
+          pickFreq.set(player.name, 0);
+        }
       }
-    }
 
-    for (let i = 0; i < players.length; i++) {
-      const element = players[i].name;
-      if (pickFreq.has(element)) {
-        const newVal = pickFreq.get(element)! + 1;
-        pickFreq.set(element, newVal);
+      for (let i = 0; i < players.length; i++) {
+        const element = players[i].name;
+        if (pickFreq.has(element)) {
+          const newVal = pickFreq.get(element)! + 1;
+          pickFreq.set(element, newVal);
+        }
       }
-    }
 
-    setEmptyPlayer(new Map(pickFreq));
-    setPlayerData(new Map(newPlayerstats));
-  };
+      setEmptyPlayer(new Map(pickFreq));
+      setPlayerData(new Map(newPlayerstats));
+    };
+    playerDataHandler();
+  }, [props.data, props.data2.Teams]);
 
   const [screenWidth, setScreenWidth] = useState(1000);
 
@@ -211,11 +212,6 @@ const Stats = (props: { data: any; data2: any }) => {
   return (
     <section className="flex min-h-screen w-full items-start justify-center">
       <div className=" ml-2 flex  w-full max-w-3xl flex-col justify-center ">
-        {!players.length && (
-          <button className="btn" onClick={() => playerDataHandler()}>
-            Generate stats
-          </button>
-        )}
         {players.length > 0 && (
           <section className="mt-2 grid grid-cols-1 gap-4">
             <h3 className="text-center text-3xl">
