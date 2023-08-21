45703e15cd7cae3bafcde0202a67c3b92aa0605d
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-08-21 01:28:28 +0100

    Update index.tsx

--- src/pages/[league]/index.tsx
+++ src/pages/[league]/index.tsx
@@ -35,7 +35,7 @@ export async function getStaticProps(paths: { params: { league: string } }) {
 }
 
 export async function getStaticPaths() {
-  // const path = "http://localhost:3000/";
+  // const path = "http://localhost:3000";
   const path = "https://esportsfantasy.app";
   const res = await fetch(`${path}/api/leagues`, { method: "GET" });
   const data = await res.json();
