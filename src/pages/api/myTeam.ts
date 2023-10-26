f650a0b494555dbbef6f961141400be3e64ccf4c
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-10-26 16:08:51 +0100

    one more place

--- src/pages/api/myTeam.ts
+++ src/pages/api/myTeam.ts
@@ -45,7 +45,7 @@ export default async function assetHandler(
                   league: true,
                   SelectedPlayer: {
                     include: {
-                      Player: { select: { playerPoints: true } },
+                      Player: { select: { playerPoints: true, image: true } },
                       bonus: true,
                     },
                   },
