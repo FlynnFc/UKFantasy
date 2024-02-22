a1cf86981bcddbdeb457f3d40401086f164f3b73
Flynn <88561679+FlynnFc@users.noreply.github.com>
2024-02-22 16:56:12 +0000

    Update index.tsx

--- src/pages/[league]/index.tsx
+++ src/pages/[league]/index.tsx
@@ -223,11 +223,14 @@ const LeaguePage = (props: { data: league; streams: stream[] }) => {
                     My Pickem
                   </button>
                 </Link>
-                <Link href={`${query.league}/pickem/stats`}>
-                  <button className="btn w-full select-none p-3 text-center font-semibold uppercase">
-                    Pickem stats
-                  </button>
-                </Link>
+                {/* <Link href={`${query.league}/pickem/stats`}> */}
+                <button
+                  disabled
+                  className="btn w-full select-none p-3 text-center font-semibold uppercase"
+                >
+                  Pickem stats
+                </button>
+                {/* </Link> */}
               </div>
             )}
             <Link href={`${query.league}/teams`}>
