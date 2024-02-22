c88b3d5c85a6ec099c727870ca333e7a2039585c
Flynn <88561679+FlynnFc@users.noreply.github.com>
2024-02-22 15:02:33 +0000

    Update pickem.ts

--- src/pages/api/pickem.ts
+++ src/pages/api/pickem.ts
@@ -11,7 +11,11 @@ const admins = async (req: NextApiRequest, res: NextApiResponse) => {
       try {
         const pickemTeam = await prisma.pickem.findUniqueOrThrow({
           where: { userId: userId },
-          include: { playoffs: true, user: { select: { name: true } } },
+          include: {
+            playoffs: true,
+            user: { select: { name: true } },
+            results: { include: { league: { select: { startDate: true } } } },
+          },
         });
 
         res.status(200).json(pickemTeam);
